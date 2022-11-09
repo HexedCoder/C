35b86f332d1eccecf48c49044d2ef07c6c486fef
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-11-09 12:43:51 -0500

    Add bst with queue for level traversal

---
+++ b_search_tree/bst_driver.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdint-gcc.h>
+#include "tree.h"
+
+void print(void *data) {
+
+    printf("%lu ", (uint64_t) data);
+}
+
+int
+person_compare(char *name_1, char *name_2) {
+
+    int index = 0;
+    while (name_1[index] == name_2[index]) {
+        ++index;
+    }
+
+    return name_2[index] - name_1[index];
+}
+
+int int_cmp(const void *input_1, const void *input_2) {
+    return (uint64_t) input_2 - (uint64_t) input_1;
+}
+
+int main(void) {
+    uint64_t arr[] = {90, 1, 8, 20, 20, 89, 13, 89, 81, 61, 62, 39, 96, 29, 93};
+//    uint64_t arr2[] = {90, 1, 8, 20, 20, 89, 13, 89, 81, 61, 62, 39, 96, 29, 93};
+//    char *arr2[] = {"Kyle", "Jacob", "Chante", "Tim", "Craig", "Sheriff",
+//                    "Jill", "Jillian"};
+
+    size_t arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    tree *tree = tree_create((int (*)(void *, void *)) int_cmp,
+                             (void (*)(void *)) print);
+    int ret = 0;
+    for (size_t i = 0; i < arr_size; ++i) {
+        ret = tree_insert(tree, (void *) arr[i]);
+        if (!ret) {
+            return 1;
+        }
+    }
+
+    printf("Preorder: ");
+    preorder(tree);
+    printf("Postorder: ");
+    postorder(tree);
+    printf("Inorder: ");
+    inorder(tree);
+    printf("Levelorder: ");
+    levelorder(tree);
+
+    printf("Size: %d\n", tree_size(tree));
+
+    void *sizes = tree_maximum(tree);
+    if (sizes) {
+        printf("Tree Max: %lu\n", (uint64_t) sizes);
+    } else {
+        printf("Tree Max: 0\n");
+    }
+    sizes = tree_minimum(tree);
+    if (sizes) {
+        printf("Tree Min: %lu\n", (uint64_t) sizes);
+    } else {
+        printf("Tree Min: 0\n");
+    }
+
+    print_visual(tree);
+
+    ret = 0;
+    for (int i = 0; i < arr_size; ++i) {
+
+        ret = tree_delete(&tree, (void *) arr[i]);
+
+        if (ret) {
+            printf("Deleted: %lu\n", arr[i]);
+            print_visual(tree);
+        }
+    }
+
+    print_visual(tree);
+    printf("Size: %d\n", tree_size(tree));
+
+    tree_destroy(&tree);
+}
---
+++ b_search_tree/tree.h
@@ -0,0 +1,63 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+typedef struct tree tree;
+
+typedef int (*compare)(void *, void *);
+
+typedef void (*action)(void *);
+
+/**
+ * @brief Function to allocate tree
+ *
+ * Provides user an address to a linked-list
+ *
+ * @return tree* On success, NULL on failure
+ */
+tree *tree_create(compare compare_func, action action_func);
+
+void print_visual(tree *root);
+
+void preorder(tree *root);
+
+void postorder(tree *root);
+
+void inorder(tree *root);
+
+void levelorder(tree *root);
+
+/**
+ * @brief Adds void* data to linked-list as stack
+ *
+ * @param p_llist Linked-list which will hold data
+ * @param data Void* being added to linked-list
+ * @return 1 On success
+ * @return 0 On failure
+ */
+int tree_insert(tree *root, void *data);
+
+void *tree_search(tree *, void *);
+
+void *tree_minimum(tree *tree);
+
+void *tree_maximum(tree *tree);
+
+/**
+ * @brief Checks size of linked-list
+ *
+ * @param p_llist Linked-list to check
+ * @return uint64_t Number of elements in linked-list
+ */
+int tree_size(tree *tree);
+
+int tree_delete(tree **root, void *val);
+
+/**
+ * @brief Function to delete linked-list and free memory
+ *
+ * @param p_llist Linked-list to delete
+ * @param destroy_data Function describing how to delete
+ */
+void tree_destroy(tree **p_tree);
+
+#endif
