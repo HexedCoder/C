f9500cf6f5eca606c050650aed4a8cffff7ccfee
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-11-15 14:45:12 -0500

    Update checks

--- linked_list/single_llist/llist.c
+++ linked_list/single_llist/llist.c
@@ -2,35 +2,30 @@
 
 #include "llist.h"
 
-typedef struct node_t
-{
-    struct node_t * next;
-    void *          data;
+typedef struct node_t {
+    struct node_t *next;
+    void *data;
 } node_t;
 
-struct llist_t
-{
-    node_t * head;
-    node_t * tail;
+struct llist_t {
+    node_t *head;
+    node_t *tail;
     uint64_t count;
 };
 
-static node_t * create_node(void * data);
+static node_t *create_node(void *data);
 
 llist_t *
-llist_create()
-{
+llist_create() {
     return calloc(1, sizeof(llist_t));
 } /* llist_create() */
 
 static node_t *
-create_node(void * data)
-{
-    node_t * node = NULL;
+create_node(void *data) {
+    node_t *node = NULL;
 
     node = calloc(1, sizeof(*node));
-    if (node)
-    {
+    if (node) {
         node->data = data;
     }
 
@@ -38,16 +33,13 @@ create_node(void * data)
 } /* create_node() */
 
 void
-llist_delete(llist_t * p_llist, void (*destroy_data)(void *))
-{
-    if (!p_llist)
-    {
+llist_delete(llist_t *p_llist, void (*destroy_data)(void *)) {
+    if (!p_llist) {
         return;
     }
 
-    node_t * curr = p_llist->head;
-    while (curr)
-    {
+    node_t *curr = p_llist->head;
+    while (curr) {
         p_llist->head = curr->next;
         destroy_data(curr->data);
         free(curr);
@@ -58,57 +50,46 @@ llist_delete(llist_t * p_llist, void (*destroy_data)(void *))
 } /* llist_delete() */
 
 int
-llist_push(llist_t * p_llist, void * data)
-{
+llist_push(llist_t *p_llist, void *data) {
     int ret = 0;
-    if (!p_llist || !data)
-    {
+    if (!p_llist || !data) {
         goto INSERT_FRONT;
     }
 
-    node_t * node = create_node(data);
-    if (!node)
-    {
+    node_t *node = create_node(data);
+    if (!node) {
         goto INSERT_FRONT;
     }
 
-    if (!p_llist->tail)
-    {
+    if (!p_llist->tail) {
         p_llist->head = node;
         p_llist->tail = node;
-        node->next    = NULL;
-    }
-    else
-    {
-        node->next    = p_llist->head;
+        node->next = NULL;
+    } else {
+        node->next = p_llist->head;
         p_llist->head = node;
     }
 
     p_llist->count++;
     ret = 1;
 
-INSERT_FRONT:
+    INSERT_FRONT:
     return ret;
 } /* llist_push() */
 
 void *
-llist_pop(llist_t * p_llist)
-{
-    void * ret = NULL;
-    if (!p_llist->head)
-    {
+llist_pop(llist_t *p_llist) {
+    void *ret = NULL;
+    if (!p_llist->head) {
         goto EXTRACT_FRONT;
     }
 
-    node_t * curr = p_llist->head;
-    ret           = curr->data;
+    node_t *curr = p_llist->head;
+    ret = curr->data;
 
-    if (curr->next)
-    {
+    if (curr->next) {
         p_llist->head = curr->next;
-    }
-    else
-    {
+    } else {
         p_llist->head = NULL;
         p_llist->tail = NULL;
     }
@@ -116,32 +97,26 @@ llist_pop(llist_t * p_llist)
 
     p_llist->count--;
 
-EXTRACT_FRONT:
+    EXTRACT_FRONT:
     return ret;
 } /* llist_pop() */
 
 int
-llist_enqueue(llist_t * p_llist, void * data)
-{
+llist_enqueue(llist_t *p_llist, void *data) {
     int ret = 0;
-    if (!p_llist || !data)
-    {
+    if (!p_llist || !data) {
         goto INSERT_BACK;
     }
 
-    node_t * node = create_node(data);
-    if (!node)
-    {
+    node_t *node = create_node(data);
+    if (!node) {
         goto INSERT_BACK;
     }
 
-    if (p_llist->tail)
-    {
+    if (p_llist->tail) {
         p_llist->tail->next = node;
-        p_llist->tail       = node;
-    }
-    else
-    {
+        p_llist->tail = node;
+    } else {
         p_llist->head = node;
         p_llist->tail = node;
     }
@@ -149,51 +124,43 @@ llist_enqueue(llist_t * p_llist, void * data)
     p_llist->count++;
     ret = 1;
 
-INSERT_BACK:
+    INSERT_BACK:
     return ret;
 } /* llist_enqueue() */
 
 void *
-llist_dequeue(llist_t * p_llist)
-{
-    void * ret = llist_pop(p_llist);
+llist_dequeue(llist_t *p_llist) {
+    void *ret = llist_pop(p_llist);
     return ret;
 } /* llist_dequeue() */
 
 int
-llist_add_before(llist_t * p_llist, void * target, void * data)
-{
+llist_add_before(llist_t *p_llist, void *target, void *data) {
     int ret = 0;
-    if (!p_llist || !data || p_llist->count == 0)
-    {
+    if (!p_llist || !data || p_llist->count == 0) {
         goto INSERT_BEFORE;
     }
 
-    node_t * node = create_node(data);
+    node_t *node = create_node(data);
 
-    if (!node)
-    {
+    if (!node) {
         goto INSERT_BEFORE;
     }
 
-    node_t * curr = p_llist->head;
+    node_t *curr = p_llist->head;
 
-    if (curr->data == target)
-    {
-        node->next    = curr;
+    if (curr->data == target) {
+        node->next = curr;
         p_llist->head = node;
-        ret           = 1;
+        ret = 1;
         goto INSERT_BEFORE;
     }
 
-    while (curr->next && curr->next->data != target)
-    {
+    while (curr->next && curr->next->data != target) {
         curr = curr->next;
     }
-    if (curr->next)
-    {
-        if (curr->next == p_llist->tail)
-        {
+    if (curr->next) {
+        if (curr->next == p_llist->tail) {
             p_llist->tail = node;
         }
         node->next = curr->next;
@@ -203,34 +170,29 @@ llist_add_before(llist_t * p_llist, void * target, void * data)
         ret = 1;
     }
 
-INSERT_BEFORE:
+    INSERT_BEFORE:
     return ret;
 } /* llist_add_before() */
 
 int
-llist_add_after(llist_t * p_llist, void * target, void * data)
-{
+llist_add_after(llist_t *p_llist, void *target, void *data) {
     int ret = 0;
-    if (!p_llist || !data || p_llist->count == 0)
-    {
+    if (!p_llist || !data || p_llist->count == 0) {
         goto INSERT_AFTER;
     }
 
-    node_t * node = create_node(data);
+    node_t *node = create_node(data);
 
-    if (!node)
-    {
+    if (!node) {
         goto INSERT_AFTER;
     }
 
-    node_t * curr = p_llist->head;
+    node_t *curr = p_llist->head;
 
-    while (curr && curr->data != target)
-    {
+    while (curr && curr->data != target) {
         curr = curr->next;
     }
-    if (curr)
-    {
+    if (curr) {
         node->next = curr->next;
         curr->next = node;
 
@@ -238,49 +200,43 @@ llist_add_after(llist_t * p_llist, void * target, void * data)
         ret = 1;
     }
 
-INSERT_AFTER:
+    INSERT_AFTER:
     return ret;
 } /* llist_add_after() */
 
 void *
-llist_extract_back(llist_t * p_llist)
-{
-    void * ret = NULL;
-    if (!p_llist->tail)
-    {
+llist_extract_back(llist_t *p_llist) {
+    void *ret = NULL;
+    if (!p_llist->tail) {
         goto EXTRACT_BACK;
     }
 
-    node_t * curr = p_llist->head;
+    node_t *curr = p_llist->head;
 
-    if (p_llist->head == p_llist->tail)
-    {
+    if (p_llist->head == p_llist->tail) {
         ret = curr->data;
 
         p_llist->head = NULL;
         p_llist->tail = NULL;
 
         free(curr);
+        p_llist->count--;
         goto EXTRACT_BACK;
     }
 
-    while (curr->next && curr->next != p_llist->tail)
-    {
+    while (curr->next && curr->next != p_llist->tail) {
         curr = curr->next;
     }
 
-    if (curr->next)
-    {
-        node_t * temp = curr;
+    if (curr->next) {
+        node_t *temp = curr;
 
-        curr       = curr->next;
+        curr = curr->next;
         temp->next = NULL;
-        ret        = curr->data;
+        ret = curr->data;
 
         p_llist->tail = temp;
-    }
-    else
-    {
+    } else {
         p_llist->head = NULL;
         p_llist->tail = NULL;
     }
@@ -288,46 +244,39 @@ llist_extract_back(llist_t * p_llist)
     free(curr);
     p_llist->count--;
 
-EXTRACT_BACK:
+    EXTRACT_BACK:
     return ret;
 } /* llist_extract_back() */
 
 void
-llist_print(llist_t * p_llist, void (*print_data)(void *))
-{
-    if (!p_llist)
-    {
+llist_print(llist_t *p_llist, void (*print_data)(void *)) {
+    if (!p_llist) {
         return;
     }
 
-    node_t * node = p_llist->head;
-    while (node)
-    {
+    node_t *node = p_llist->head;
+    while (node) {
         print_data(node->data);
         node = node->next;
     }
 } /* llist_print() */
 
 void *
-llist_find(llist_t * p_llist,
-           void *    target,
-           int (*compare_data)(void * initial, void * comparison))
-{
-    if (!p_llist || !target)
-    {
+llist_find(llist_t *p_llist,
+           void *target,
+           int (*compare_data)(void *initial, void *comparison)) {
+    if (!p_llist || !target) {
         return NULL;
     }
 
-    void *   data = NULL;
-    node_t * node = p_llist->head;
-    int      ret;
+    void *data = NULL;
+    node_t *node = p_llist->head;
+    int ret;
 
-    while (node)
-    {
+    while (node) {
         ret = compare_data(target, node->data);
 
-        if (!ret)
-        {
+        if (!ret) {
             data = node->data;
             break;
         }
@@ -337,7 +286,6 @@ llist_find(llist_t * p_llist,
 } /* llist_find() */
 
 uint64_t
-get_size(llist_t * p_llist)
-{
+get_size(llist_t *p_llist) {
     return p_llist->count;
 } /* get_size() */
---
+++ linked_list/single_llist/llist_test.c
@@ -0,0 +1,251 @@
+/** @file llist_check.c
+ *
+ * @brief llist_check.c tests public functions from llist.h
+ *
+ * COPYRIGHT NOTICE: (c) 2022 Jacob Hitchcox
+ *
+ */
+
+#include "llist.h"
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t node_t;
+
+struct node_t {
+    char value;
+};
+
+int char_cmp(void *char_1, void *char_2) {
+    return *(char *) char_2 - *(char *) char_1;
+}
+
+struct node_t *
+node_create(char value) {
+    node_t *node = calloc(1, sizeof(*node));
+
+    if (!value) {
+        goto PERSON_CREATE_EXIT;
+    }
+
+    if (node) {
+        node->value = value;
+    }
+    PERSON_CREATE_EXIT:
+    return node;
+}
+
+llist_t *setup_stack(const char *string, size_t len) {
+    llist_t *stack = llist_create();
+    if (!stack) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; ++i) {
+        if (string[i] == '*') {
+            if (get_size(stack)) {
+                void *value = llist_pop(stack);
+                free(value);
+            } else {
+                break;
+            }
+        } else {
+            node_t *node = node_create(string[i]);
+            llist_push(stack, node);
+        }
+    }
+    return stack;
+}
+
+llist_t *setup_queue(const char *string, size_t len) {
+    llist_t *queue = llist_create();
+    if (!queue) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; ++i) {
+        if (string[i] == '*') {
+            if (get_size(queue)) {
+                void *value = llist_dequeue(queue);
+                free(value);
+            } else {
+                break;
+            }
+        } else {
+            node_t *node = node_create(string[i]);
+            llist_enqueue(queue, node);
+        }
+    }
+    return queue;
+}
+
+START_TEST(test_valid_llist_create) {
+    llist_t *stack = llist_create();
+    ck_assert(stack != NULL);
+    llist_delete(stack, (void (*)(void *)) free);
+}
+
+END_TEST
+
+START_TEST(test_valid_stack_operation) {
+    const char *valid_string = "THIS IS A VALID STRING";
+    size_t len = strlen(valid_string);
+    llist_t *stack = setup_stack(valid_string, len);
+
+    for (int i = 0; i < len; ++i) {
+        node_t *curr = (node_t *) llist_pop(stack);
+        ck_assert(curr->value == valid_string[len - 1 - i]);
+        free(curr);
+    }
+    ck_assert(get_size(stack) == 0);
+    llist_delete(stack, (void (*)(void *)) free);
+}
+
+END_TEST
+
+START_TEST(test_valid_queue_operation) {
+    const char *valid_string = "THIS IS A VALID STRING";
+    size_t len = strlen(valid_string);
+    llist_t *queue = setup_queue(valid_string, len);
+
+    for (int i = 0; i < len; ++i) {
+        node_t *curr = (node_t *) llist_dequeue(queue);
+        ck_assert(curr->value == valid_string[i]);
+        free(curr);
+    }
+    ck_assert(get_size(queue) == 0);
+    llist_delete(queue, (void (*)(void *)) free);
+}
+
+END_TEST
+
+START_TEST(test_valid_extract_back_operation) {
+    const char *valid_string = "THIS IS A VALID STRING";
+    size_t len = strlen(valid_string);
+    llist_t *llist = setup_queue(valid_string, len);
+
+    for (int i = 0; i < len; ++i) {
+        node_t *curr = (node_t *) llist_extract_back(llist);
+        ck_assert(curr->value == valid_string[len - 1 - i]);
+        free(curr);
+    }
+
+    ck_assert(get_size(llist) == 0);
+    llist_delete(llist, (void (*)(void *)) free);
+}
+
+END_TEST
+
+START_TEST(test_valid_find_operation) {
+    const char *valid_string = "THIS IS A VALID STRING";
+    size_t len = strlen(valid_string);
+    size_t search_idx = 6;
+
+    llist_t *llist = setup_queue(valid_string, len);
+
+    node_t *curr = (node_t *) llist_find(llist,
+                                         (void *) &valid_string[search_idx],
+                                         char_cmp);
+    ck_assert(curr->value == valid_string[search_idx]);
+
+    llist_delete(llist, (void (*)(void *)) free);
+}
+
+END_TEST
+
+START_TEST(test_invalid_stack_operation) {
+    llist_t *stack = llist_create();
+    node_t *curr = (node_t *) llist_pop(stack);
+    ck_assert_ptr_null(curr);
+    free(stack);
+}
+
+END_TEST
+
+START_TEST(test_invalid_queue_operation) {
+    llist_t *stack = llist_create();
+    node_t *curr = (node_t *) llist_dequeue(stack);
+    ck_assert_ptr_null(curr);
+    free(stack);
+}
+
+END_TEST
+
+START_TEST(test_invalid_extract_back_operation) {
+    llist_t *stack = llist_create();
+    node_t *curr = (node_t *) llist_extract_back(stack);
+    ck_assert_ptr_null(curr);
+    free(stack);
+}
+
+END_TEST
+
+START_TEST(test_invalid_find_operation) {
+    const char *valid_string = "THIS IS A VALID STRING";
+    size_t len = strlen(valid_string);
+    char search_char = 'Z';
+
+    llist_t *llist = setup_queue(valid_string, len);
+
+    node_t *curr = (node_t *) llist_find(llist,
+                                         (void *) &search_char,
+                                         char_cmp);
+    ck_assert_ptr_null(curr);
+
+    llist_delete(llist, (void (*)(void *)) free);
+}
+
+END_TEST
+
+Suite *
+check_llist(void) {
+    Suite *suite;
+
+    TCase *tc_valid, *tc_invalid, *tc_all;
+
+    suite = suite_create("llist_tests");
+
+    tc_valid = tcase_create("Valid");
+    tcase_set_tags(tc_valid, "Valid");
+
+    tcase_add_test(tc_valid, test_valid_llist_create);
+    tcase_add_test(tc_valid, test_valid_stack_operation);
+    tcase_add_test(tc_valid, test_valid_queue_operation);
+    tcase_add_test(tc_valid, test_valid_extract_back_operation);
+    tcase_add_test(tc_valid, test_valid_find_operation);
+
+    tc_invalid = tcase_create("Invalid");
+    tcase_set_tags(tc_invalid, "Invalid");
+
+    tcase_add_test(tc_invalid, test_invalid_stack_operation);
+    tcase_add_test(tc_invalid, test_invalid_queue_operation);
+    tcase_add_test(tc_invalid, test_invalid_extract_back_operation);
+    tcase_add_test(tc_invalid, test_invalid_find_operation);
+
+    tc_all = tcase_create("All");
+    tcase_set_tags(tc_all, "All");
+
+    suite_add_tcase(suite, tc_valid);
+    suite_add_tcase(suite, tc_invalid);
+    suite_add_tcase(suite, tc_all);
+
+    return suite;
+}
+
+int
+main() {
+    Suite *suite = check_llist();
+    SRunner *runner = srunner_create(suite);
+
+    srunner_set_fork_status(runner, CK_NOFORK);
+
+//    srunner_run_tagged(runner, NULL, NULL, "Valid", NULL, CK_VERBOSE);
+    srunner_run_all(runner, CK_VERBOSE);
+
+    int no_failed = srunner_ntests_failed(runner);
+
+    srunner_free(runner);
+    return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+/*** end of file***/
