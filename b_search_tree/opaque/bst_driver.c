2eae10f3129d639edc1c52b256a9093fe0583971
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-11-29 13:32:51 -0500

    Use struct instead of int

--- b_search_tree/opaque/bst_driver.c
+++ b_search_tree/opaque/bst_driver.c
@@ -1,91 +1,120 @@
 #include <stdio.h>
 #include <string.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <stdint-gcc.h>
-#include "avl.h"
+#include "tree.h"
 
-void print_num(void *data) {
-
-    printf("%lu ", (uint64_t) data);
+void print_num(void *data)
+{
+	printf("%lu ", (uint64_t)data);
 }
 
-void print_str(void *data) {
-
-    printf("%s ", (char *) data);
+void print_str(void *data)
+{
+	person *tmp = (person *)data;
+	printf("%d ", tmp->age);
 }
 
-int
-person_compare(char *name_1, char *name_2) {
+int person_compare(person *person_1, person *person_2)
+{
+	const char *name_1 = person_1->ssn;
+	const char *name_2 = person_2->ssn;
 
-    int index = 0;
-    while (name_1[index] == name_2[index] && name_2[index]) {
-        ++index;
-    }
+	int index = 0;
+	while (name_1[index] == name_2[index] && name_2[index]) {
+		++index;
+	}
 
-    return name_2[index] - name_1[index];
+	return name_2[index] - name_1[index];
 }
 
-int int_cmp(const void *input_1, const void *input_2) {
-    return (uint64_t) input_2 - (uint64_t) input_1;
+int int_cmp(const void *input_1, const void *input_2)
+{
+	return (uint64_t)input_2 - (uint64_t)input_1;
 }
 
-int main(void) {
-//    uint64_t arr[] = {90, 1, 8, 20, 20, 89, 13, 89, 81, 61, 62, 39, 96, 29, 93};
-    char *arr[] = {"Kyle", "Jacob", "Chante", "Tim", "Craig", "Sheriff",
-                   "Jill", "Jillian"};
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("No file name provided\n");
+		return 1;
+	}
+	FILE *file;
+	char *file_name = argv[1];
+	file = fopen(file_name, "r");
+	if (!file) {
+		printf("Invalid file\n");
+		return 1;
+	}
 
-    size_t arr_size = sizeof(arr) / sizeof(arr[0]);
+	tree *tree = tree_create((int (*)(void *, void *))person_compare,
+				 (void (*)(void *))print_str);
+	int ret;
+	int count = 1;
+	char buffer[1024];
+	while (fgets(buffer, 1024, file)) {
+		person *tmp = calloc(1, sizeof(person));
 
-    tree *tree = tree_create((int (*)(void *, void *)) person_compare,
-                             (void (*)(void *)) print_str);
-    int ret = 0;
-    for (size_t i = 0; i < arr_size; ++i) {
-        ret = tree_insert(tree, (void *) arr[i]);
-        if (!ret) {
-            return 1;
-        }
-    }
-    printf("Arr Size: %zu\n", arr_size);
+		char *fields[8];
+		char *p = buffer;
 
-    printf("Preorder: ");
-    preorder(tree);
-    printf("Postorder: ");
-    postorder(tree);
-    printf("Inorder: ");
-    inorder(tree);
-    printf("Levelorder: ");
-    levelorder(tree);
+		for (int n = 0; n < 5; n++) {
+			// save the pointer to the start of the field
+			fields[n] = p;
+			// skip to the end of the field
+			p += strcspn(p, "\t\n");
+			if (*p != '\0') {
+				// set a null terminator for the field and skip to the next one
+				*p++ = '\0';
+			}
+		}
+		tmp->ssn = strdup(fields[0]);
+		tmp->fname = strdup(fields[1]);
+		tmp->lname = strdup(fields[2]);
 
-    printf("Size: %d\n", tree_size(tree));
+		tmp->age = strtol(fields[3], NULL, 10);
+		tmp->income = strtol(fields[4], NULL, 10);
 
-    void *sizes = tree_maximum(tree);
-    if (sizes) {
-        printf("Tree Max: %s\n", (char *) sizes);
-    } else {
-        printf("Tree Max: 0\n");
-    }
-    sizes = tree_minimum(tree);
-    if (sizes) {
-        printf("Tree Min: %s\n", (char *) sizes);
-    } else {
-        printf("Tree Min: 0\n");
-    }
+		ret = tree_insert(tree, tmp);
+		if (!ret) {
+			return 1;
+		}
+	}
+	fclose(file);
 
-    print_visual(tree);
+	printf("Arr Size: %d\n", count);
 
-    ret = 0;
-    for (int i = 0; i < arr_size; ++i) {
+	printf("Preorder: ");
+	preorder(tree);
+	printf("Postorder: ");
+	postorder(tree);
+	printf("Inorder: ");
+	inorder(tree);
+	printf("Level order: ");
+	levelorder(tree);
 
-        ret = tree_delete(&tree, (void *) arr[i]);
+	printf("Size: %d\n", tree_size(tree));
 
-        if (ret) {
-            printf("Deleted: %s\n", arr[i]);
-            print_visual(tree);
-        }
-    }
+	void *sizes = tree_maximum(tree);
+	if (sizes) {
+		printf("Tree Max: %s\n", (char *)sizes);
+	} else {
+		printf("Tree Max: 0\n");
+	}
+	sizes = tree_minimum(tree);
+	if (sizes) {
+		printf("Tree Min: %s\n", (char *)sizes);
+	} else {
+		printf("Tree Min: 0\n");
+	}
 
-    print_visual(tree);
-    printf("Size: %d\n", tree_size(tree));
+	int income = get_income(tree);
+	printf("Average Income: %d\n", income);
 
-    tree_destroy(&tree);
+	income = get_age_income(tree, 62, 62);
+	printf("Average Income between 50-59: %d\n", income);
+
+	//	print_visual(tree);
+
+	tree_destroy(&tree);
 }
