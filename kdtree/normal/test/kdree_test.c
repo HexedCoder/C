7507e7c5d8c0a5322c930cc389eb3739764b30bd
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-12-15 14:13:43 -0500

    Add additional test cases and create function for testing code

--- kdtree/normal/test/kdree_test.c
+++ kdtree/normal/test/kdree_test.c
@@ -13,6 +13,79 @@
 
 #define MAX_LINE_LENGTH 1024
 
+int get_data(FILE *file)
+{
+	int line_no = 0;
+	int count = 1;
+	char buffer[1024];
+	char *broken = NULL;
+	tree *bst = NULL;
+
+	while (fgets(buffer, 1023, file)) {
+		char *fields[2];
+		char *ptr = buffer;
+
+		for (int i = 0; i < 2; i++) {
+			// save the pointer to the start of the field
+			fields[i] = ptr;
+			// skip to the end of the field
+			ptr += strcspn(ptr, ",\n");
+			if (*ptr != '\0') {
+				// set a null terminator for the field and skip to the next one
+				*ptr++ = '\0';
+			}
+		}
+
+		double tmp_x_coord = strtod(fields[0], &broken);
+		if (*broken || fields[0] == broken) {
+			if (0 == line_no++) {
+				continue;
+			} else {
+				fprintf(stderr, "%s broke me\n", buffer);
+				fclose(file);
+				tree_delete(&bst);
+				return 1;
+			}
+		}
+		double tmp_y_coord = strtod(fields[1], &broken);
+
+		if (*broken || fields[1] == broken) {
+			fprintf(stderr, "%s broke me\n", buffer);
+			fclose(file);
+			tree_delete(&bst);
+			return 2;
+		}
+
+		if (fabs(tmp_x_coord) > 180 || fabs(tmp_y_coord) > 180) {
+			printf("Coordinate must be between -180 and 180 "
+			       "degrees\n");
+			fclose(file);
+			tree_delete(&bst);
+			return 3;
+		}
+
+		if (1 == count++) {
+			bst = create_tree_node(tmp_x_coord, tmp_y_coord);
+
+			if (!bst) {
+				return 1;
+			}
+		} else {
+			int ret = kd_insert(
+				bst, create_tree_node(tmp_x_coord, tmp_y_coord),
+				0);
+			if (!ret) {
+				fprintf(stderr, "Unable to insert into tree\n");
+				fclose(file);
+				tree_delete(&bst);
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
 START_TEST(test_valid_kdtree_ops)
 {
 	char valid_string[] = "test/sample_valid_input";
@@ -44,82 +117,47 @@ START_TEST(test_valid_kdtree_ops)
 
 END_TEST START_TEST(test_invalid_kdtree_ops)
 {
-	const char *invalid_string = "test/sample_empty_input";
-	ck_assert(read_file(invalid_string) == NULL);
-
-	const char *invalid_string2 = "test/sample_invalid_input5";
-	FILE *file = read_file(invalid_string2);
-
-	char buffer[MAX_LINE_LENGTH] = { 0 };
-	int label_index = 0;
-	char *broken = NULL;
-	int empty = 1;
 	int ret = 0;
+	const char *empty_file_string = "test/sample_empty_input";
+	ck_assert(read_file(empty_file_string) == NULL);
 
-	while (fgets(buffer, 1023, file)) {
-		char *fields[2];
-		char *ptr = buffer;
-		tree *bst = NULL;
+	const char *invalid_string = "test/sample_invalid_input";
+	FILE *file = read_file(invalid_string);
+	ret = get_data(file);
 
-		for (int i = 0; i < 2; i++) {
-			// save the pointer to the start of the field
-			fields[i] = ptr;
-			// skip to the end of the field
-			ptr += strcspn(ptr, ",\n");
-			if (*ptr != '\0') {
-				// set a null terminator for the field and skip to the next one
-				*ptr++ = '\0';
-			}
-		}
-
-		double tmp_x_coord = strtod(fields[0], &broken);
-
-		if (*broken || fields[0] == broken) {
-			if (0 == label_index++) {
-				continue;
-			} else {
-				printf("broken: %d\n", *broken);
-				fprintf(stderr, "%s broke me\n", buffer);
-				ret = 1;
-				break;
-			}
-		}
-		double tmp_y_coord = strtod(fields[1], &broken);
-
-		if (*broken || fields[1] == broken) {
-			printf("broken: %d\n", *broken);
-			fprintf(stderr, "%s broke me\n", buffer);
-			ret = 1;
-			break;
-		}
-
-		if (fabs(tmp_x_coord) > 180 || fabs(tmp_y_coord) > 180) {
-			ret = 1;
-			printf("Each coordinate must be between -180 and 180 "
-			       "degrees\n");
-			break;
-		}
-		if (1 == empty--) {
-			bst = create_tree_node(tmp_x_coord, tmp_y_coord);
-			if (!bst) {
-				ret = 1;
-				break;
-			}
-		} else {
-			ret =
-			    kd_insert(bst,
-				      create_tree_node(tmp_x_coord,
-						       tmp_y_coord), 0);
-			if (!ret) {
-				fprintf(stderr, "Unable to insert into tree\n");
-				fclose(file);
-				tree_delete(&bst);
-				break;
-			}
-		}
-	}
-	// Improper coordinate input
+	// Invalid x_coord input
 	ck_assert(ret == 1);
+
+	const char *invalid_string2 = "test/sample_invalid_input2";
+	file = read_file(invalid_string2);
+	ret = get_data(file);
+
+	// Invalid x_coord input
+	ck_assert(ret == 3);
+
+	const char *invalid_string3 = "test/sample_invalid_input3";
+	file = read_file(invalid_string3);
+	ret = get_data(file);
+
+	// Invalid x_coord input
+	ck_assert(ret == 3);
+
+	const char *invalid_string4 = "test/sample_invalid_input4";
+	file = read_file(invalid_string4);
+	ret = get_data(file);
+
+	// Invalid y_coord input
+	ck_assert(ret == 3);
+
+	const char *invalid_string5 = "test/sample_invalid_input5";
+	file = read_file(invalid_string5);
+	ret = get_data(file);
+
+	// Invalid y_coord input
+	ck_assert(ret == 3);
+
+	// Null tree input
+	ck_assert(tree_size(NULL) == 0);
 }
 
 END_TEST Suite *kdtree_check(void)
