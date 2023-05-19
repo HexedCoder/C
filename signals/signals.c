f47ed28909158a2d7eb9deeb4516ea01f94608ef
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-05-19 08:00:05 -0400

    Update with comments

--- signals/signals.c
+++ signals/signals.c
@@ -20,9 +20,7 @@ int establish_handler(int signum, handler_func func)
 
 	int result = 0;
 
-	if (signum == SIGWINCH) {
-		sa.sa_flags = SA_RESTART;
-	}
+	sa.sa_flags = SA_RESTART;
 
 	if (-1 == sigaction(signum, &sa, NULL)) {
 		perror("sigaction");
@@ -41,6 +39,10 @@ void block_signals(int num_signals, ...)
 
 	sigset_t mask;
 
+	// Retrieve the current signal mask
+	sigprocmask(0, NULL, &mask);
+
+	// Create empty signal set
 	sigemptyset(&mask);
 
 	va_list signal_list;
@@ -49,11 +51,13 @@ void block_signals(int num_signals, ...)
 	for (int i = 0; i < num_signals; i++) {
 		int signum = va_arg(signal_list, int);
 
+		// Add signal to block to set
 		sigaddset(&mask, signum);
 	}
 
 	va_end(signal_list);
 
+	// Set the mask
 	sigprocmask(SIG_BLOCK, &mask, NULL);
 }				/* block_signals() */
 
@@ -64,34 +68,45 @@ void unblock_signals(int num_signals, ...)
 	}
 
 	sigset_t mask;
-	sigemptyset(&mask);
+
+	// Retrieve the current signal mask
+	sigprocmask(0, NULL, &mask);
 
 	va_list signal_list;
 	va_start(signal_list, num_signals);
 
 	for (int i = 0; i < num_signals; i++) {
 		int signum = va_arg(signal_list, int);
+
+		// Add signals to remove
 		sigaddset(&mask, signum);
 	}
 
 	va_end(signal_list);
 
+	// Set the mask
 	sigprocmask(SIG_UNBLOCK, &mask, NULL)
 }				/* unblock_signals() */
 
 void block_all_signals(void)
 {
 	sigset_t mask;
+
+	// Add all signals to mask
 	sigfillset(&mask);
 
+	// Set the mask
 	sigprocmask(SIG_BLOCK, &mask, NULL);
 }				/* block_all_signals() */
 
 void unblock_all_signals(void)
 {
 	sigset_t mask;
+
+	// Remove signals from mask
 	sigemptyset(&mask);
 
+	// Set the mask
 	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }				/* unblock_all_signals() */
 
